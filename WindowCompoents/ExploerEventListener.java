0d14a3b7da992cc808554f8e6e8f228cbe82210e
Damian Recoskie <damian_programmer@hotmail.com>
2021-03-05 00:47:07 -0500

    Update

--- WindowCompoents/ExploerEventListener.java
+++
@@ -1,9 +0,0 @@
-package WindowCompoents;
-
-import RandomAccessFileV.*;
-
-public interface ExploerEventListener
-{
-  public void elementOpen(String Element);
-  public void read(String File, RandomAccessFileV file);
-}
