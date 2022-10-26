5ae999dd2b1d3a887cf6012a9d3723bbcf7f0fbc
Damian Recoskie <damian_programmer@hotmail.com>
2022-10-26 10:41:32 -0400

    Update

--- Format/test.js
+++ Format/test.js
@@ -10,27 +10,24 @@ format = {
     //Test the new binary tree component.
     
     var root = new treeNode("test.exe");
-    root.add("Header1.h");
-    root.add("Header2.h");
-    var im = new treeNode("Import.dll");
+    root.add("Header1.h", ["h1"]);
+    root.add("Header2.h", ["h2",0]);
+    var im = new treeNode("Import.dll"); root.add(im);
     im.add("Func1");
     im.add("Func2");
-    var sf = new treeNode("SubFunc");
+    var sf = new treeNode("SubFunc"); im.add(sf);
     sf.add("SubFunc1");
     sf.add("SubFunc2");
     sf.add("SubFunc3");
     sf.add("SubFunc4");
-    var rs = new treeNode("Resource");
+    var rs = new treeNode("Resource"); root.add(sf);
     rs.add("File1");
     rs.add("File2");
-    var fl = new treeNode("Folder");
+    var fl = new treeNode("Folder"); sf.add(fl);
     fl.add("File1");
     fl.add("File2");
     fl.add("File3");
-    fl.add("File4",'test');
-    
-    im.add(sf); root.add(im);
-    rs.add(fl); root.add(rs);
+    fl.add("File4", ["test"]);
 
     Tree.set(root); tree.prototype.event = this.event;
   },
@@ -39,6 +36,25 @@ format = {
 
   event: function(node)
   {
-    info.innerHTML = "You click on node " + node.innerText + "<br /><br />Node changeable or settable attributes = " + node.getAttribute("args") + "<br /><br />The attributes can tell our format reader where to go in the file or what to read and decode on a node click.";
+    var args = node.getArgs();
+    
+    //Dynamically change the tree.
+    
+    if(args[0] == "h1")
+    {
+      var n = new treeNode("Header1.h");
+      n.add("info1");
+      n.add("info2");
+      node.setNode(n);
+    }
+    
+    //set or change attributes.
+    
+    if(args[0] == "h2")
+    {
+      args[1] += 1; node.setArgs(args);
+    }
+    
+    info.innerHTML = "You click on node " + node.innerText + "<br /><br />Node changeable or settable attributes = " + args + "<br /><br />The attributes can tell our format reader where to go in the file or what to read and decode on a node click.";
   }
 }
